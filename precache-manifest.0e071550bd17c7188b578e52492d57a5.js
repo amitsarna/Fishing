808fe287e5573f4b2086a005b876436fe44ab06b
amitsarna <amit.sarna.584@gmail.com>
2020-04-27 13:13:33 +0100

    Updates

---
+++ precache-manifest.0e071550bd17c7188b578e52492d57a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee789681f25710fd2b135280ea216ab3",
+    "url": "/Fishing/index.html"
+  },
+  {
+    "revision": "ee7b27cb063cb657fd40",
+    "url": "/Fishing/static/css/main.d1f2d643.chunk.css"
+  },
+  {
+    "revision": "b8cfc27b693318886148",
+    "url": "/Fishing/static/js/2.50d4defc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fishing/static/js/2.50d4defc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7b27cb063cb657fd40",
+    "url": "/Fishing/static/js/main.119e204a.chunk.js"
+  },
+  {
+    "revision": "a2f6c2b5237c0c8c3a0b",
+    "url": "/Fishing/static/js/runtime-main.ecd8ba14.js"
+  }
+]);
